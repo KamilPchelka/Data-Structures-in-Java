c9382e55a61d394f044f081207549a58a413d995
siemaeniu500 <siemaeniu500@gmail.com>
2018-04-25 12:37:22 +0200

    Implement unit tests for SinglyLinkedList class.

--- src/test/java/pl/kamilpchelka/codecool/datastructures/MySinglyLinkedListTest.java
+++ src/test/java/pl/kamilpchelka/codecool/datastructures/MySinglyLinkedListTest.java
@@ -1,7 +1,5 @@
 package pl.kamilpchelka.codecool.datastructures;
 
-import org.hamcrest.CoreMatchers;
-import org.hamcrest.core.Is;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -10,8 +8,9 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.core.Is.*;
-import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 public class MySinglyLinkedListTest {
 
@@ -46,6 +45,74 @@ public class MySinglyLinkedListTest {
 
         assertThat(mySinglyLinkedList.toString(), is(equalTo("5")));
 
+    }
+
+
+    @Test
+    public void prepend_testIfPrependingWorksWhenListHasManyElements() {
+
+        data.forEach(mySinglyLinkedList::append);
+        mySinglyLinkedList.prepend(9);
+
+        assertThat(mySinglyLinkedList.toString().charAt(0), is(equalTo('9')));
+
+    }
+
+    @Test
+    public void delete_testIfDeletingWorksWithOneElementInTheList() {
+
+        int value = 5;
+
+        mySinglyLinkedList.append(value);
+        mySinglyLinkedList.delete(value);
+
+        assertThat(mySinglyLinkedList.toString(), is(equalTo("")));
+    }
+
+    @Test
+    public void delete_testIfDeletingWorksWithManyElementsInTheList() {
+
+
+        data.forEach(mySinglyLinkedList::append);
+        mySinglyLinkedList.delete(5);
+        mySinglyLinkedList.delete(4);
+
+
+        assertThat(mySinglyLinkedList.toString(), is(equalTo("1,2,3")));
+    }
+
+    @Test
+    public void getLast_returnsNullIfListIsEmpty() {
+
+        assertThat(mySinglyLinkedList.getLast(), is(nullValue()));
+    }
+
+    @Test
+    public void getLast_returnsHeadWhenListContainsOneelement() {
+
+        int value = 5;
+
+        mySinglyLinkedList.append(value);
+
+        assertThat(mySinglyLinkedList.getLast(), is(value));
+    }
+
+    @Test
+    public void getLast_returnsLastElementAfterAppending() {
+        int value = 6;
+
+        data.forEach(mySinglyLinkedList::append);
+        mySinglyLinkedList.append(value);
+
+        assertThat(mySinglyLinkedList.getLast(), is(value));
+    }
+
+    @Test
+    public void map_affectsAllEmenetsInTheList() {
+        data.forEach(mySinglyLinkedList::append);
+
+        mySinglyLinkedList.map(integer -> ++integer);
+        assertThat(mySinglyLinkedList.toString(), is(equalTo("2,3,4,5,6")));
     }
 
 }
