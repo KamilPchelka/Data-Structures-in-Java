a7d624d34cff29a59fc9415f7360f23d3c9df278
siemaeniu500 <siemaeniu500@gmail.com>
2018-04-25 12:39:09 +0200

    Implement SinglyLinkedList class.

--- src/main/java/pl/kamilpchelka/codecool/datastructures/MySinglyLinkedList.java
+++ src/main/java/pl/kamilpchelka/codecool/datastructures/MySinglyLinkedList.java
@@ -3,13 +3,33 @@ package pl.kamilpchelka.codecool.datastructures;
 import java.util.Iterator;
 import java.util.Spliterator;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
-public class MySinglyLinkedList<T> implements MyList<T>{
+public class MySinglyLinkedList<T> implements MyList<T> {
 
     private SingleNode<T> head;
+    private SingleNode<T> last;
+    private int length = 0;
 
     @Override
     public void delete(T value) {
+
+        if (head == null) return;
+
+        if (head.value == value) {
+            head = null;
+            return;
+        }
+
+        SingleNode current = head;
+
+        while (current.next != null) {
+            if (current.next.value == value) {
+                current.next = current.next.next;
+                return;
+            }
+            current = current.next;
+        }
 
     }
 
@@ -17,6 +37,8 @@ public class MySinglyLinkedList<T> implements MyList<T>{
     public void append(T value) {
         if (head == null) {
             head = new SingleNode<>(value);
+            last = head;
+            length++;
             return;
         }
 
@@ -25,8 +47,10 @@ public class MySinglyLinkedList<T> implements MyList<T>{
         while (current.next != null) {
             current = current.next;
         }
+        SingleNode<T> newNode = new SingleNode<>(value);
+        current.next = newNode;
+        last = newNode;
 
-        current.next = new SingleNode<>(value);
     }
 
     @Override
@@ -39,37 +63,59 @@ public class MySinglyLinkedList<T> implements MyList<T>{
         SingleNode<T> singleNode = new SingleNode<>(value);
         singleNode.next = head;
         head = singleNode;
+        length++;
     }
 
     @Override
     public String toString() {
         final StringBuilder result = new StringBuilder();
-        if (head.next == null) {
+        if (head == null) {
+            return "";
+        } else if (head.next == null) {
             result.append(head);
         } else {
-            forEach(e -> result.append(e).append(","));
+            forEach((Consumer<? super T>) e -> result.append(e).append(","));
             result.deleteCharAt(result.length() - 1);
         }
         return result.toString();
     }
 
     @Override
-    public Iterator iterator() {
+    public Iterator<T> iterator() {
         return null;
     }
 
     @Override
-    public void forEach(Consumer action) {
-        SingleNode current = head;
+    public void forEach(Consumer<? super T> action) {
+        SingleNode<T> current = head;
+
         while (current != null) {
-            action.accept(current);
+            action.accept(current.value);
+            current = current.next;
+
+        }
+    }
+
+    public void map(Function<T, T> function) {
+        SingleNode<T> current = head;
+
+        while (current != null) {
+            current.value = function.apply(current.value);
             current = current.next;
 
         }
     }
 
     @Override
-    public Spliterator spliterator() {
+    public Spliterator<T> spliterator() {
         return null;
+    }
+
+    public T getLast() {
+        return last == null ? null : last.value;
+    }
+
+    public int getLength() {
+        return length;
     }
 }
