9e43e7f1e902855c62c922df59c6e17e797dbaea
kamil <siemaeniu500@gmail.com>
2018-05-15 11:34:23 +0200

    Add throwing QueueException to enqueue method signature

--- src/main/java/pl/kamilpchelka/codecool/datastructures/queue/Queue.java
+++ src/main/java/pl/kamilpchelka/codecool/datastructures/queue/Queue.java
@@ -1,8 +1,10 @@
 package pl.kamilpchelka.codecool.datastructures.queue;
 
+import pl.kamilpchelka.codecool.datastructures.exceptions.QueueFullException;
+
 public interface Queue<T> {
 
-    void enqueue(T value);
+    void enqueue(T value) throws QueueFullException;
 
     T peek();
 
