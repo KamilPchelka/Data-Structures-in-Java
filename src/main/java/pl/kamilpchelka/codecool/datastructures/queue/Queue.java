405fef2129dbf85836d5ee0a170c6dc1db344b96
kamil <siemaeniu500@gmail.com>
2018-05-15 10:04:19 +0200

    Create Queue interface.

---
+++ src/main/java/pl/kamilpchelka/codecool/datastructures/queue/Queue.java
@@ -0,0 +1,16 @@
+package pl.kamilpchelka.codecool.datastructures.queue;
+
+public interface Queue<T> {
+
+    void enqueue(T value);
+
+    T peek();
+
+    T dequeue();
+
+    int queueSize();
+
+    boolean isEmpty();
+
+
+}
